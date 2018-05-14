464fa09d17d0570bb6ce1b5f1b8aec320bf34713
Royserg <staniojakub@gmail.com>
2018-05-14 23:07:13 +0300

    showing sidebar, usernameinput showing name

--- static/js/app.js
+++ static/js/app.js
@@ -1,42 +1,57 @@
+let usernameNavbar = document.querySelector('.brand-logo');
+const usernameInput = document.querySelector('#usernameInput'); 
+
 // When DOM loaded
 document.addEventListener('DOMContentLoaded', function() {
-
+    
     // init username modal
     const usernameModal = document.querySelector('#usernameModal');
     const modalOptions = {
-        onOpenEnd: () => document.querySelector('#usernameInput').focus()
+        onOpenEnd: () => {
+            usernameInput.value = usernameNavbar.textContent;
+            usernameInput.focus();
+        }
     };
     
     const uModal = M.Modal.init(usernameModal, modalOptions);
     
     // if username not in localStorage
-    // if (localstorage....)
-    uModal.open();
+    if (!localStorage.getItem('username'))
+        uModal.open();
+    else
+        usernameNavbar.innerHTML = localStorage.getItem('username');
 
     // set input keyup events
     document.querySelector('#usernameInput').onkeyup = changeUsername;
     // action when hit save btn in modal
     document.querySelector('#saveUsername').onclick = changeUsername;
 
+
+    // init sidenav
+    const sideNav = document.querySelector('.sidenav');
+    const sideN = M.Sidenav.init(sideNav);
+
+
   });
 
+  const changeUsername = (e) => {
+      let source = e.originalTarget || e.srcElement ;
+  
+      if (e.key === 'Enter' || source.id === "saveUsername") {
+          // target username input
+          if (usernameInput.value.length > 0) {
+              // change username on page
+              usernameNavbar.innerHTML = usernameInput.value;
+              // save username in localStorage
+              localStorage.setItem('username', usernameInput.value);
+              // clear input field
+              usernameInput.value = '';
+              // close modal
+              M.Modal.getInstance(usernameModal).close();
+  
+          } else {
+              alert('Provide an username')
+          }
+      }
+  }
 
-const changeUsername = (e) => {
-    let source = e.originalTarget || e.srcElement ;
-
-    if (e.key === 'Enter' || source.id === "saveUsername") {
-        // target username input
-        const username = document.querySelector('#usernameInput');
-
-        if (username.value.length > 0) {
-            document.querySelector('.brand-logo').innerHTML = username.value;
-            // clear input field
-            username.value = '';
-            // close modal
-            M.Modal.getInstance(usernameModal).close();
-
-        } else {
-            alert('Provide an username')
-        }
-    }
-}
