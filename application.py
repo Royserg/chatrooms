6066f61c00aefda2c51233531c45985c63fb302a
Royserg <staniojakub@gmail.com>
2018-05-15 23:54:10 +0300

    emit sending msg and broadcasting

--- application.py
+++ application.py
@@ -7,11 +7,45 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 socketio = SocketIO(app)
 
+chatrooms = {
+    "Testing": [
+        {
+            "text": "First Message",
+            "author": "Jakub",
+            "date": "15.05.2018"
+        },
+        {
+            "text": "Hello Test",
+            "author": "Dariusz",
+            "date": "15.05.2018"
+        }
+    ],
+    "Second": [
+        {
+            "text": "Second Test",
+            "author": "Jakub",
+            "date": "16.05.2018"
+        }
+    ]
+}
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', chatrooms=chatrooms)
 
+
+@app.route('/api/<chatroom>', methods=['GET'])
+def get_conversation(chatroom):
+    return chatroom 
+
+
+@socketio.on('send msg')
+def message(data):
+    chatroom = data['chatroom']
+    # update global variable
+    chatrooms[chatroom].append({ 'text': data['text'], 'author': data['author'], 'date': data['date'] })
+    emit('announce msg', data, broadcast=True)
 
 
 
--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,7 @@
 let usernameNavbar = document.querySelector('.brand-logo');
 const usernameInput = document.querySelector('#usernameInput'); 
+const chatInput = document.querySelector('#chatInput');
+const msgForm = document.querySelector('#msgForm');
 
 // When DOM loaded
 document.addEventListener('DOMContentLoaded', function() {
@@ -27,31 +29,74 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#saveUsername').onclick = changeUsername;
 
 
+    // TODO: setup currentChat var in localStorage
+
     // init sidenav
     const sideNav = document.querySelector('.sidenav');
     const sideN = M.Sidenav.init(sideNav);
 
+    // Connect to websocket
+    var socket = io.connect(`${location.protocol}//${document.domain}:${location.port}`);
 
+    // When connected, configure message input
+    socket.on('connect', () => {
+
+        // submiting form () should emit "send msg" event
+        msgForm.onsubmit = () => {
+            let text = chatInput.value;
+            
+            // TODO: pull info for msg from LocalStorage
+            let msg = {
+                'chatroom': 'Testing',
+                'author': 'Jakub',
+                'text': text,
+                'date': Date.now()
+            }
+            
+            socket.emit('send msg', msg)
+            // clear chat input
+            chatInput.value = "";
+            // prevent form from default behavior
+            return false;
+        }
+    })
+
+    // When a new message is announced, add to the conversation if opened this chatroom
+    socket.on('announce msg', data => {
+        // check with localStorage current chatroom
+        if (data.chatroom === 'Testing') {
+            const div = document.createElement('div');
+            div.innerHTML = data.text;
+            div.classList.add('card-panel');
+            div.classList.add('chat-bubble');
+            document.querySelector('.chat-messages').append(div);
+        }
+    })
+
+    
   });
 
-  const changeUsername = (e) => {
-      let source = e.originalTarget || e.srcElement ;
-  
-      if (e.key === 'Enter' || source.id === "saveUsername") {
-          // target username input
-          if (usernameInput.value.length > 0) {
-              // change username on page
-              usernameNavbar.innerHTML = usernameInput.value;
-              // save username in localStorage
-              localStorage.setItem('username', usernameInput.value);
-              // clear input field
-              usernameInput.value = '';
-              // close modal
-              M.Modal.getInstance(usernameModal).close();
-  
-          } else {
-              alert('Provide an username')
-          }
-      }
-  }
+
+
+
+const changeUsername = (e) => {
+    let source = e.originalTarget || e.srcElement ;
+
+    if (e.key === 'Enter' || source.id === "saveUsername") {
+        // target username input
+        if (usernameInput.value.length > 0) {
+            // change username on page
+            usernameNavbar.innerHTML = usernameInput.value;
+            // save username in localStorage
+            localStorage.setItem('username', usernameInput.value);
+            // clear input field
+            usernameInput.value = '';
+            // close modal
+            M.Modal.getInstance(usernameModal).close();
+
+        } else {
+            alert('Provide an username')
+        }
+    }
+}
 
